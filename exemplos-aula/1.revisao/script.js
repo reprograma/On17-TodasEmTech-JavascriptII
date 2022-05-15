06ad4cfbb2a0aa1c8dd716c89b38e6ca39d5b164
viviane.miranda <viviane.miranda@nubank.com.br>
2022-05-15 16:39:39 -0300

    adicionando exemplos da aula e excs

--- exemplos-aula/1.revisao/script.js
+++ exemplos-aula/1.revisao/script.js
@@ -1,1 +1,34 @@
 //retomando .map() e .filter()
+
+const alunasOn17 = [
+    {
+        nome: "Lara Oliveira",
+        idade: 28,
+        cidade: "Belém"
+    },
+    {
+        nome: "Nicolle Dias",
+        idade: 29,
+        cidade: "Blumenau"
+
+    },
+    {
+        nome: "Mayra Pereira",
+        idade: 30,
+        cidade: "Guarujá"
+    },
+    {
+        nome: "Emanueli Martins",
+        idade: 24,
+        cidade: "Praia Grande"
+    },
+];
+
+
+//filtrar apenas as alunas que sejam maiores de 25 anos
+alunasOn17.filter(banana => banana >25);
+
+//mapear cada aluna, adicionando 1 ano de idade para cada uma delas
+const alunasOn17idade2023 = alunasOn17.map(item => item.idade+1);
+
+alunasOn17.map( (item, indiceDoItem) => console.log(indiceDoItem));
