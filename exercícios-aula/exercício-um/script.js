c9861ea844dd9a163c265477ef7d33da680de93c
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2022-05-17 13:00:50 -0300

    first commit - exercício 1

---
+++ exercícios-aula/exercício-um/script.js
@@ -0,0 +1,19 @@
+// Exercício 1
+let divPrimeira = document.querySelector('#primeiraDiv');
+let divSegunda = document.querySelector("#segundaDiv");
+let divTerceira = document.querySelector("#terceiraDiv");
+let paragraphs = document.querySelector("p");
+let paragraphContent = paragraphs.textContent;
+
+//paragraphs.innerHTML = "Novo Lore Ipsum!";
+//divPrimeira.style.backgroundColor = "2px solid red";
+// divSegunda.style.backgroundColor="#6342f5";
+// divTerceira.style.backgroundColor="#a142f5";
+
+
+console.log(paragraphs);
+console.log(paragraphContent);
+
+
+
+
