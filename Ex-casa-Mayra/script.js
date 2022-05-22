f98322e7c12d1f49970bd7482dca6f921b44bd82
Mayra Pereira <maypereira@live.com>
2022-05-21 21:22:15 -0300

    Exercicios 1,2,3 e 4

---
+++ Ex-casa-Mayra/script.js
@@ -0,0 +1,25 @@
+const headerPrincipal = document.querySelector("header");
+headerPrincipal.style.fontFamily = "Odibee Sans, cursive";
+
+const primeiroTitulo = document.querySelector("h1");
+primeiroTitulo.style.color = "red";
+primeiroTitulo.style.backgroundColor = "white";
+primeiroTitulo.style.textShadow = "1px 1px black";
+primeiroTitulo.style.fontSize = "5rem";
+primeiroTitulo.style.textAlign = "center";
+
+const segundoTutulo = document.querySelector("h2")
+segundoTutulo.style.margin ="0 3rem"
+segundoTutulo.style.fontSize = "2rem"
+
+const primeiroParagrafo = document.querySelector(".section-header1");
+primeiroParagrafo.style.backgroundColor = "#737373";
+primeiroParagrafo.style.border = "2px solid black";
+primeiroParagrafo.style.margin = "1rem";
+primeiroParagrafo.style.padding = "1rem 2rem";
+primeiroParagrafo.style.fontSize = "1rem"
+ 
+const primeiraDiv = document.querySelector("primeira-div");
+/*const imagemMatrix = document.createElement("img");
+primeiraDiv.appendChild(imagemMatrix);
+imagemLuisaSonza.style.width = "600px"; */
