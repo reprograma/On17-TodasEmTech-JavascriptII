45d669cfbc45edede8ff97a78e91aafdfdae0f15
Thayanne Andrade <thayannesandrade@gmail.com>
2022-05-24 20:48:23 -0300

    estilizando o botao

--- exercícios-aula/thayanne-andrade/ex-3/script.js
+++ exercícios-aula/thayanne-andrade/ex-3/script.js
@@ -3,6 +3,17 @@ const body = document.body;
 const form = document.querySelector("form");
 const botao = document.querySelector("form button");
 
+botao.style.width = "120px";
+botao.style.height = "80px";
+botao.style.padding = "12px";
+botao.style.color = "#fff";
+botao.style.fontSize = "18px";
+botao.style.fontWeight = "bold";
+botao.style.fontFamily = "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', sans-serif";
+botao.style.backgroundColor = "#7f7fff";
+botao.style.borderRadius = "8px";
+botao.style.border = "none";
+
 function enviar(evento) {
     evento.preventDefault();
     const img = document.createElement("img");
