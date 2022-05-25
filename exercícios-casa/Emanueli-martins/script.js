7ebbddb282c61254f5d50cf18d69d692fabe6a97
Emanueli Martins <emanueli.martiins@gmail.com>
2022-05-25 20:31:22 -0300

    começando/tentando os exercicios

---
+++ exercícios-casa/Emanueli-martins/script.js
@@ -0,0 +1,4 @@
+const titulo =document.querySelector("Reprograma")
+const artigo = document.querySelector("p")
+const imagem = document.querySelector("img")
+const link = document.querySelector("a")
