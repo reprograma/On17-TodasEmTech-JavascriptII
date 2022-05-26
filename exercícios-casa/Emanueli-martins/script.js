4274b37d0f57e005ce295f573b367af84c65357a
Emanueli Martins <emanueli.martiins@gmail.com>
2022-05-26 19:58:23 -0300

    resolução atividade

--- exercícios-casa/Emanueli-martins/script.js
+++
@@ -1,4 +0,0 @@
-const titulo =document.querySelector("Reprograma")
-const artigo = document.querySelector("p")
-const imagem = document.querySelector("img")
-const link = document.querySelector("a")
