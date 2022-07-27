fc97d17a6ab35aeb00b745d1359c1c62e3a03314
Danielle Bená <dani.bena@hotmail.com>
2022-07-27 03:21:36 -0300

    entrega - danielle bená

--- exercícios-aula/script2.js
+++
@@ -1,9 +0,0 @@
-/*
-- Escolha 2 elementos que um que será removido e um adicionado pelo DOM.
-- Adicione algum evento ao 3o elemento. Esse evento deverá ser visível ao usuário.
-- o 5o elemento deverá ser filho do 4o, utilize algum método relacional do DOM para alterar algum atributo CSS desse filho e desse pai.
-*/
-
-const
-
-
